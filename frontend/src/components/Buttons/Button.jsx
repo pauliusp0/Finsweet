ccccf88451fff045d00dcfc580e0fadb16ed34ba
pauliusp0 <ppuidokas420@gmail.com>
2021-10-19 15:21:47 +0300

    created button component

---
+++ frontend/src/components/Buttons/Button.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const PrimaryBtn = styled.button`
+  background-color: ${({ primary, secondary }) =>
+    primary ? 'var(--primary-color)' : secondary ? 'var(--black-color)' : null};
+  border-radius: 12px;
+  padding: 16px 40px;
+  border: none;
+  color: #ffffff;
+  font-family: Manrope;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 175%;
+`;
+
+const Button = ({ primary, secondary, text }) => {
+  return (
+    <PrimaryBtn primary={primary} secondary={secondary}>
+      {text}
+    </PrimaryBtn>
+  );
+};
+
+export default Button;
