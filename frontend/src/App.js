f0b4ff6e563b2207fa13754544eac66ad1541f74
pauliusp0 <83448583+pauliusp0@users.noreply.github.com>
2021-10-20 12:53:11 +0300

    Revert "created navbar design and routing logic"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,5 @@
-import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 function App() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' />
-          <Route path='/about-us' />
-          <Route path='/services' />
-          <Route path='/blog' />
-          <Route path='/contact' />
-        </Switch>
-      </Router>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
