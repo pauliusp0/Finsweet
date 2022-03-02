83f13701ef5f4e77b8d69c5dfd0a679d3b816794
pauliusp0 <83448583+pauliusp0@users.noreply.github.com>
2022-03-02 15:48:23 +0200

    Revert "Feature frontend 8"

--- frontend/src/components/Global.style.jsx
+++ frontend/src/components/Global.style.jsx
@@ -74,8 +74,3 @@ export const Caption = styled.p`
 
   color: #5b5b5b;
 `;
-
-export const Container = styled.section`
-  max-width: 1300px;
-  margin: 0 auto;
-`;
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,44 +1,33 @@
 import React from 'react';
+import Logo from '../../assets/Logo.png';
+import DialIcon from '../../assets/Assistance-Icon.svg';
 import styled from 'styled-components';
-import { Body01, Caption, Container } from '../Global.style';
+import { Body01, Caption } from '../Global.style';
 import { Link } from 'react-router-dom';
 
-// icons
-import Logo from '../../assets/Logo.svg';
-import DialIcon from '../../assets/Assistance-Icon.svg';
-
 const StyledNavbar = styled.header`
-  > section {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 90px;
-  }
+  max-width: 1300px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  height: 90px;
+  align-items: center;
 `;
 
 const Line = styled.div`
   opacity: 0.36;
+
   /* Grey */
   border: 1px solid #939191;
   transform: rotate(90deg);
   width: 42px;
-  margin: 12px;
 `;
 
 const Assistance = styled.div`
-  width: 181px;
-  height: 56px;
   display: flex;
   gap: 14px;
-  align-items: center;
-  margin-right: 20px;
-
   p {
     opacity: 1;
-  }
-  img {
-    width: 40px;
-    height: 40px;
   }
 `;
 
@@ -49,8 +38,7 @@ const NavbarRight = styled.div`
     display: flex;
     li {
       list-style: none;
-      /* padding: 20px; */
-      padding-left: 40px;
+      padding: 20px;
       /* Body 02 */
       font-family: Manrope;
       font-style: normal;
@@ -69,40 +57,38 @@ const NavbarRight = styled.div`
 const Navbar = () => {
   return (
     <StyledNavbar>
-      <Container>
-        <div>
-          <img src={Logo} alt='' />
-        </div>
-        <NavbarRight>
-          <nav>
-            <ul>
-              <li>
-                <Link to='/'>Home</Link>
-              </li>
-              <li>
-                <Link to='/about-us'>About us</Link>
-              </li>
-              <li>
-                <Link to='/services'>Services</Link>
-              </li>
-              <li>
-                <Link to='/blog'>Blog</Link>
-              </li>
-              <li>
-                <Link to='/contact'>Contact</Link>
-              </li>
-            </ul>
-          </nav>
-          <Line />
-          <Assistance>
-            <img src={DialIcon} alt='' />
-            <div>
-              <Caption>Road Assistance</Caption>
-              <Body01>1800 265 24 52</Body01>
-            </div>
-          </Assistance>
-        </NavbarRight>
-      </Container>
+      <div>
+        <img src={Logo} alt='' />
+      </div>
+      <NavbarRight>
+        <nav>
+          <ul>
+            <li>
+              <Link>Home</Link>
+            </li>
+            <li>
+              <Link>About us</Link>
+            </li>
+            <li>
+              <Link>Services</Link>
+            </li>
+            <li>
+              <Link>Blog</Link>
+            </li>
+            <li>
+              <Link>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <Line />
+        <Assistance>
+          <img src={DialIcon} alt='' />
+          <div>
+            <Caption>Road Assistance</Caption>
+            <Body01>1800 265 24 52</Body01>
+          </div>
+        </Assistance>
+      </NavbarRight>
     </StyledNavbar>
   );
 };
